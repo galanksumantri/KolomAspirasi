4274af897b2626e09b5e9f1097e1e2fabfdbddc7
galanksumantri <galanksumantri@gmail.com>
2023-03-17 18:42:36 +0700

    separate code

---
+++ js/index.js
@@ -0,0 +1,24 @@
+function downFunction() {
+    window.scrollTo(0, document.body.scrollHeight);
+}
+function upFunction() {
+    window.scrollTo(document.body.scrollHeight, 0);
+}
+function search() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+};
